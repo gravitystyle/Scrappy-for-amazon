5d7f0f74d9532a16cf63a8b6d95f1ac99dae091e
mehdi <mehdiboulet@hotmail.com>
2018-11-02 13:45:42 +0100

    Ajout de la fonction pour creer la liste des urls Amazon Ajout de la fonction pour exporter une liste dans un fichier au format json

--- readFile.py
+++ readFile.py
@@ -1,6 +1,5 @@
 import json
 
-values = []
 with open('categories.list') as file:
 	data = json.load(file)
 	print(data)
--- scrap.py
+++ scrap.py
@@ -2,40 +2,54 @@ import scrapy
 import json
 
 
-# def main():
-#     name = 'blogspider'
-#     start_urls = ['https://www.amazon.fr/s/ref=nb_sb_noss_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=mug+harry+potter']
-
-#     for title in response.css('div#topDynamicContent span#s-result-count'):
-#     	print(title.css('span ::text').extract_first())
-
-def cleanText(text):
-	words = text.split()
-
-
 def readFile(fileName): #extract a list from a json file -- use to create a list of category and licenses
 	with open(fileName,'r') as file:
 		data = json.load(file)
 	return data
 
+def createAmazonUrls(categories, licenses): # create the research Urls links for Amazon --- Have to be changed for another website
+	baseUrl = "https://www.amazon.fr/s/ref=nb_sb_noss_1?__mk_fr_FR=ÅMÅŽÕÑ&url=search-alias%3Daps&field-keywords=" #This is the base use for the Amazon Url
+	urls = []
+	wordsToSearch = []
+	for category in categories:
+		for license in licenses:
+			wordsToSearch = [] #Reinitialize the words to search
+			categorySplited = category.lower().split() #Create a list of words from the string of the category
+			licenseSplited = license.lower().split() #Create a list of words from the string of the license
+			wordsToSearch.extend(categorySplited) #add the list of category words to the words to search
+			wordsToSearch.extend(licenseSplited) #add the list of license words to the words to search
+			urls.append(baseUrl + "+".join(wordsToSearch)) #Create the url and add it to the list of urls
+	return urls
 
 
-class BlogSpider(scrapy.Spider):
-	name = 'blogspider'
+def exportInJson(fileName, data): # export the data in a json file
+	with open(fileName,'w') as f:
+		f.write(json.dumps(data))
 
-	def start_requests(self):
+
+
+# class BlogSpider(scrapy.Spider):
+# 	name = 'blogspider'
+
+# 	def start_requests(self):
 		
-		urls = [
-			'https://www.amazon.fr/s/ref=a9_asi_1?rh=i%3Aaps%2Ck%3Amug+batman&keywords=mug+batman',
-			'https://www.amazon.fr/s/ref=nb_sb_noss_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=porte+cl%C3%A9+batman',
-			'https://www.amazon.fr/s/ref=nb_sb_noss?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=tartendkzjqsldjk'
-		] #URL to scrap
+# 		urls = [
+# 			'https://www.amazon.fr/s/ref=a9_asi_1?rh=i%3Aaps%2Ck%3Amug+batman&keywords=mug+batman',
+# 			'https://www.amazon.fr/s/ref=nb_sb_noss_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=porte+cl%C3%A9+batman',
+# 			'https://www.amazon.fr/s/ref=nb_sb_noss?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=tartendkzjqsldjk'
+# 		] #URL to scrap
 
-		for url in urls:
-			yield scrapy.Request(url=url, callback=self.parse)
+# 		for url in urls:
+# 			yield scrapy.Request(url=url, callback=self.parse)
 
-	def parse(self, response):
-		for title in response.css('div#topDynamicContent'):#scrap the website page
-			yield {"test":title.css('span ::text').extract_first()} # extract the sentence
+# 	def parse(self, response):
+# 		for title in response.css('div#topDynamicContent'):#scrap the website page
+# 			yield {"test":title.css('span ::text').extract_first()} # extract the sentence
 
 
+######Test the collection of categories and licenses####
+# cat = readFile('categories.json')
+# lic = readFile('licenses.json')
+# urls = createAmazonUrls(cat,lic)
+# exportInJson("testUrl.json",urls)
+# print (len(urls))
